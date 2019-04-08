f3c418d3f53efaaf1ebcf32632e89b17f9c89f70
carlej <carlej@oregonstate.edu>
2019-04-08 00:09:58 -0700

    testing

--- Register.php
+++ Register.php
@@ -2,18 +2,8 @@
 <!doctype html>
 <html>
 	<head>
-		<link href="CSS/Master.css" rel="stylesheet" type="text/css" />
-		<link href="CSS/Menu.css" rel="stylesheet" type="text/css" />
-		<meta charset="utf-8">
-<!-- jQuery -->
-		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<?php include("Connections/req.php"); ?>
 
-<!-- MenuMaker Plugin -->
-		<script src="https://s3.amazonaws.com/menumaker/menumaker.min.js"></script>
-
-<!-- Icon Library -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<!-- Scaling for phones -->
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 
 		<title>Untitled Document</title>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="header"></div>
 	<div id="cssmenu" class="align-center">
  		<ul>
-     		<li><a href="#">Home</a></li>
+     		<li><a href="##">Home</a></li>
 			<li><a href="#">Bank of the SDN</a></li>
 			<li><a href="#">Land Grants</a></li>
 			<li><a href="#">Volunteering</a></li>
