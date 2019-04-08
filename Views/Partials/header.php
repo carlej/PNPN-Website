0410c9abefc2ff660ee4e13ae6f2449225d1e55b
carlej <carlej@oregonstate.edu>
2019-04-08 00:30:03 -0700

    nav

--- Views/Partials/header.php
+++ Views/Partials/header.php
@@ -3,9 +3,9 @@
 	<div id="cssmenu" class="align-center">
  		<ul>
      		<li><a href="/SDN-Website">Home</a></li>
-			<li><a href="#">Bank of the SDN</a></li>
-			<li><a href="#">Land Grants</a></li>
-			<li><a href="#">Volunteering</a></li>
+			<li><a href="/SDN-Website/bank.php">Bank of the SDN</a></li>
+			<li><a href="/SDN-Website/landgrant.php">Land Grants</a></li>
+			<li><a href="/SDN-Website/volunteer.php">Volunteering</a></li>
 			<li><a href="/SDN-Website/login.php">Login</a></li>
 		</ul>
 	</div>
---
+++ bank.php
@@ -0,0 +1,14 @@
+<?php require 'Connections/connections.php'; ?>
+<!doctype html>
+<html>
+	<head>
+		<?php include("Connections/req.php"); ?>
+
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+
+		<title>Bank</title>
+		<?php include("Views\Partials/header.php");?>
+	</head>
+	<body>This</body>
+
+</html>
