2c40ab63e7d3569b815ec2fcf35acf17e7236dea
carlej <carlej@oregonstate.edu>
2019-04-08 00:18:17 -0700

    main

---
+++ header.php
@@ -0,0 +1,12 @@
+<div class="container">
+	<div class="header"></div>
+	<div id="cssmenu" class="align-center">
+ 		<ul>
+     		<li><a href="/">Home</a></li>
+			<li><a href="#">Bank of the SDN</a></li>
+			<li><a href="#">Land Grants</a></li>
+			<li><a href="#">Volunteering</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require 'Connections/connections.php'; ?>
+<!doctype html>
+<html>
+	<head>
+		<?php include("Connections/req.php"); ?>
+
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+
+		<title>main</title>
+		<?php include("Views\Partials/header.php");?>
+	</head>
+
+</html>
