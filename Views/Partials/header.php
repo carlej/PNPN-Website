2e6433a94e341dc808d0ce1c5679d2dfde03be7b
carlej <carlej@oregonstate.edu>
2019-04-08 00:24:04 -0700

    moving

--- Views/Partials/header.php
+++ Views/Partials/header.php
@@ -2,11 +2,11 @@
 	<div class="header"></div>
 	<div id="cssmenu" class="align-center">
  		<ul>
-     		<li><a href="##">Home</a></li>
+     		<li><a href="/SDN-Website">Home</a></li>
 			<li><a href="#">Bank of the SDN</a></li>
 			<li><a href="#">Land Grants</a></li>
 			<li><a href="#">Volunteering</a></li>
-			<li><a href="#">Login</a></li>
+			<li><a href="/SDN-Website/login.php">Login</a></li>
 		</ul>
 	</div>
 </div>
