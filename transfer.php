45fff728a7ff5fca3669b13c4f8e1af959f3c834
carlej <carlej@oregonstate.edu>
2019-04-12 00:38:00 -0700

    small clean up

--- transfer.php
+++ transfer.php
@@ -28,32 +28,9 @@
 		$row=mysqli_fetch_row($resultIn);
 		$parsed_json = json_decode($row[0], true);
 		$parsed_json = $parsed_json['id'];
-//		foreach($parsed_json as $value)
-//		{
-//			$accountQuery = "SELECT Ballance FROM accounts WHERE ID = '$value'";
-//			$result = mysqli_query($con, $accountQuery);
-//			$row2=mysqli_fetch_row($result);
-//			//echo $value . "\r\n";		//$value displays that account number of the account
-//   			//echo $row2[0] . "\r\n";	//$row2[0] displayes the ballance of the account
-//		}
 		?>
 		<?php if ($username!=NULL): ?>
-		<?php foreach ($parsed_json as $value): ?>
-			<?php if($value): ?>
-			<div class="container">
-				<div id="cssmenu" class="align-center">
-					<ul>
-						<li><a><?php echo $value; ?></a></li>
-						<li><a><?php
-						$accountQuery = "SELECT Ballance FROM accounts WHERE ID = '$value'";
-						$result = mysqli_query($con, $accountQuery);
-						$row2=mysqli_fetch_row($result);
-						echo $row2[0]; ?></a></li>
-					</ul>
-				</div>
-			</div>
-		<?php endif; ?>
-		<?php endforeach; ?>
+		<?php include "Views/Partials/showAccs.php" ?>
 		<?php include("Javascript/transcript.php"); ?>
 	<?php endif; ?>
 	</body>
