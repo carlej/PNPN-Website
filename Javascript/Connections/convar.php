d35f141c8a50b1b79b9cc3a71261c9e6a1f7f401
carlej <carlej@oregonstate.edu>
2020-02-10 19:11:16 -0800

    removed

--- Javascript/Connections/convar.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', 'Pinda4656');
-  define('DB_NAME', 'PNPN-Website');
-  define('CON_STRING', 'mysql:host=localhost;dname=root');
-?>
