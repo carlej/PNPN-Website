bf2bbdb95a985155728ec0df8e786c70357920be
carlej <carlej@oregonstate.edu>
2020-02-18 01:32:35 -0800

    making clear

---
+++ Javascript/clearsome.php
@@ -0,0 +1,11 @@
+<?php 
+session_start(); 
+$_SESSION['multsearch']=array('1');
+$_SESSION['stype']=NULL;
+$_SESSION['nest']="hold";
+$_SESSION['nstype']=NULL;
+//echo $_SESSION['clear'];
+$temp=$_SESSION['clear'];
+//echo $temp;
+header('Location: '.$temp);
+?>
