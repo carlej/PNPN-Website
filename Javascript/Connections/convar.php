d39f6aba8b0359bd3fc18ff5918cde69f2a48a0d
carlej <carlej@oregonstate.edu>
2020-02-25 00:15:25 -0800

    tt

--- Javascript/Connections/convar.php
+++ Javascript/Connections/convar.php
@@ -5,4 +5,9 @@
   define('DB_PASSWORD', 'Pinda4656');
   define('DB_NAME', 'PNPN-Website');
   define('CON_STRING', 'mysql:host=localhost;dname=root');
+
+//  define('DB_NAME', 'KeyConnections');
+//  define('DB_HOST', 'unknown');
+//  define('DB_USER', 'generalcarle');
+//  define('CON_STRING', 'mysql:host=unknown;dname=generalcarle');
 ?>
