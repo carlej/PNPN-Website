1922a3d3707a795327cfdc4f87b787bb2dbd8950
carlej <carlej@oregonstate.edu>
2020-02-28 00:30:18 -0800

    fixed register

--- login.php
+++ login.php
@@ -95,7 +95,7 @@
         			<input type = "reset"  value = "Clear" style="font-family: pirates" />
     			</p>
 				<div id="buttons" class="align-center" style="font-family: pirates">
-     				<a href="/PNPN-Website/register.php" style="color:black; text-decoration: none;">Register Here</a>
+     				<a href="/PNPN-Website/Register.php" style="color:black; text-decoration: none;">Register Here</a>
      			</div>
 			</form>
 			</div>
