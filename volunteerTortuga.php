1c7747788482f0ea7b4a5d0dab412b4f0e385006
carlej <carlej@oregonstate.edu>
2020-03-14 02:51:25 -0700

    tortuga

--- volunteerTortuga.php
+++ volunteerTortuga.php
@@ -8,7 +8,7 @@
 			$username = $_SESSION['username'];
 		}
 		include 'Javascript/Connections/convar.php';
-		$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		$con = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 		if (!$con) {
 			die('Could not connect: ' . mysql_error());
 		}
